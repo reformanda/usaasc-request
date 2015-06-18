c1a638943926b68effda5e67e9e8cd7aa489aac4
reformanda <working1008@gmail.com>
2015-06-17 22:20:19 -0400

    fix the landing page; cleanup sigin/sign up styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :tos_checked
+  before_filter :require_tos
+ 
+
+private
+  def tos_checked
+    @tos_checked = session["tos_confirmation"]
+  end
+
+  def require_tos
+    unless tos_checked 
+      redirect_to landing_index_path
+      return false
+    end
+  end
+
+
 end
--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,13 +1,15 @@
 class LandingController < ApplicationController
 
-
-
 	def index
 	end
 
 	def create
 		session["tos_confirmation"] = true
 		redirect_to action: "index"
+	end
+
+	def require_tos
+	  return true
 	end
 
 end
