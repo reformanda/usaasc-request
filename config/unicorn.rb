97cccf4508a0f60c2bd72cf1e5efb2292a3c72f8
reformanda <working1008@gmail.com>
2015-07-09 09:59:45 -0400

    change unicorn settings

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,8 +14,8 @@
 # Listen on a UNIX data socket
 #listen "/tmp/unicorn.gifroll.sock"
 
-# 16 worker processes for production environment
-worker_processes 16
+# 1 worker processes for production environment
+worker_processes 1
 
 # Load rails before forking workers for better worker spawn time
 preload_app true
