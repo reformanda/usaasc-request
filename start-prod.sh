bf6779493d00576e289449a8ce1287f218125638
james <jwilson1@networkrunners.com>
2015-07-16 12:07:58 -0400

    added production scripts

---
+++ start-prod.sh
@@ -0,0 +1,11 @@
+export ADMIN_NAME="James Wilson" 
+export ADMIN_PASSWORD=mystuff02 
+export DOMAIN_NAME=networkrunners.com 
+export ADMIN_EMAIL=jwilson1@networkrunners.com 
+export SECRET_KEY_BASE=9ef9c6bd1471aac37057b806d92222ac0f018e555322f8a7d97cf 
+export RAILS_ENV=production 
+
+thin start
+
+
+
