b1221228cbb9151a3017b88be6f3dd66b2286fad
reformanda <working1008@gmail.com>
2015-07-22 23:05:26 -0400

    set default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,10 @@ Rails.application.configure do
     #password: Rails.application.secrets.email_provider_password
   }
 
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { host: 'helpdesk-testing.usaasconesource.info' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
