ed08053d15644d62ce419c95b48205ada113b58f
reformanda <working1008@gmail.com>
2015-07-26 23:29:08 -0400

    remove terms of service screen/landing page; configure reply-to email for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :tos_checked
-  before_filter :require_tos, :set_user_time_zone
+  before_filter :set_user_time_zone
   #before_filter: set_user_time_zone
 
 
--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,7 +2,7 @@ class LandingController < ApplicationController
 
 	def index
 	  if !user_signed_in?
-	    #redirect_to new_user_session_path
+	    redirect_to new_user_session_path
 	  else
 	  	redirect_to requests_path
 	  end 	
